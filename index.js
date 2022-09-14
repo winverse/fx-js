21abbd48c741ae62056c178186df038153380902
sangHyun <public.winverse@gmail.com>
2022-09-15 06:52:11 +0900

    Remove unnecessary code

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-const {
-  filter,
-  delay,
-  map,
-  go,
-  pipe,
-  range,
-  log,
-  reduce,
-  sum,
-} = require("./hard");
+const H = require("./hard");
 const L = require("./lazy");
 const C = require("./concurrency");
 
-L.interval = (time) => L.map(delay(time), L.range(Infinity));
-
-console.time("concurrency");
-go(
-  range(10000),
-  C.filter((a) => a % 2),
-  C.map((a) => a * a),
-  C.take(2),
-  C.reduce(sum),
-  log
-);
-console.timeEnd("concurrency");
-
-// module.exports = { L, C, ...H };
+module.exports = { L, C, ...H };
