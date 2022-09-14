61fcf1eab76ddce5210e173c6286ca5d659a5758
sangHyun <public.winverse@gmail.com>
2022-09-15 00:04:19 +0900

    Add strict/sum

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const stricts = require("./strict");
+const { go, filter, map, reduce, sum, log } = require("./strict");
 
-console.log(stricts);
+const result = go(
+  [1, 2, 3, 4, 5],
+  filter((a) => a % 2), // [1,3,5]
+  map((a) => a * a), // [1, 9, 25]
+  reduce(sum)
+);
+
+log(result); // 35
